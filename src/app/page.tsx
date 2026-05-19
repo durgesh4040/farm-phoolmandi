76598605ad2339040684c6affc34e7dadb6c7651
durgesh <durgesh404011@gmail.com>
2026-05-20 01:55:26 +0530

    about pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-
 import HeroSection from "@/components/sections/herosection"
 import MarqueeStrip from "@/components/ui/MarqueeStrip"
 import CategoriesSection from "@/components/sections/categorysection"
-import AboutSection from "@/components/sections/aboutsection"
 import FeaturedFlowers from "@/components/sections/featuredflower"
 import BulkOrdersSection from "@/components/sections/bulkordersection"
 import BookingSection from "@/components/sections/bookingsection"
@@ -10,15 +8,13 @@ import TestimonialsSection from "@/components/sections/testimonialsection"
 export default function Page() {
   return (
     <>
-        <HeroSection />
-        <MarqueeStrip />
-        <CategoriesSection />
-        <FeaturedFlowers />
-        <BulkOrdersSection />
-        <BookingSection />
-        <TestimonialsSection />
-    
-
+      <HeroSection />
+      <MarqueeStrip />
+      <CategoriesSection />
+      <FeaturedFlowers />
+      <BulkOrdersSection />
+      <BookingSection />
+      <TestimonialsSection />
       {/* <Footer /> */}
     </>
   )
