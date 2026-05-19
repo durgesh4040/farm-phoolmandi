d2f636680b4f9fdc86df4c3c7465229cff395219
durgesh <durgesh404011@gmail.com>
2026-05-20 02:37:06 +0530

    update flower page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,11 @@
 import HeroSection from "@/components/sections/herosection"
-import MarqueeStrip from "@/components/ui/MarqueeStrip"
-import CategoriesSection from "@/components/sections/categorysection"
-import FeaturedFlowers from "@/components/sections/featuredflower"
-import BulkOrdersSection from "@/components/sections/bulkordersection"
-import BookingSection from "@/components/sections/bookingsection"
-import TestimonialsSection from "@/components/sections/testimonialsection"
 export default function Page() {
   return (
     <>
+      <div>
       <HeroSection />
-      <MarqueeStrip />
-      <CategoriesSection />
-      <FeaturedFlowers />
-      <BulkOrdersSection />
-      <BookingSection />
-      <TestimonialsSection />
       {/* <Footer /> */}
+      </div>
     </>
   )
 }
